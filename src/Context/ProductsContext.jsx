5c0789daf076a91779965e1f97b1ebbe6b2a7fdf
jayad23 <jaidervanegas@live.com>
2021-11-23 09:00:27 -0500

    styles changes

--- src/Context/ProductsContext.jsx
+++ src/Context/ProductsContext.jsx
@@ -1,12 +1,12 @@
 import React, { createContext, useReducer } from 'react'
 import Amatista1 from '../Imgs/Amatista1.jpg'
-import Amatista2 from '../Imgs/Amatista2.png'
+import Amatista2 from '../Imgs/Amatista2.jpg'
 import Amatista3 from '../Imgs/Amatista3.png'
-import Cristal1 from '../Imgs/Cristal1.png'
-import Cristal2 from '../Imgs/Cristal2.png'
+import Cristal1 from '../Imgs/Cristal1.jpg'
+import Cristal2 from '../Imgs/Cristal2.jpg'
 import Cristal3 from '../Imgs/Cristal3.jpg'
 import QuarzoRosa1 from '../Imgs/QuarzoRosa1.jpg'
-import QuarzoRosa2 from '../Imgs/QuarzoRosa2.png'
+import QuarzoRosa2 from '../Imgs/QuarzoRosa2.jpg'
 import QuarzoRosa3 from '../Imgs/QuarzoRosa3.png'
 import Hydrafacial1 from '../Imgs/Hydrafacial1.jpg'
 import Hydrafacial2 from '../Imgs/Hydrafacial2.png'
@@ -17,6 +17,9 @@ import Tonic3 from '../Imgs/Tonic3.png'
 import Drenaje1 from '../Imgs/Drenaje1.jpg'
 import Drenaje2 from '../Imgs/Drenaje2.png'
 import Drenaje3 from '../Imgs/Drenaje3.png'
+import DeepCl1 from '../Imgs/DeepCl1.png'
+import DeepCl2 from '../Imgs/DeepCl2.jpg'
+import DeepCl3 from '../Imgs/DeepCl3.jpg'
 
 const ProductsContext = createContext()
 
@@ -74,13 +77,39 @@ const ProductsContextProvider = ({children}) => {
                 price: 150000,
                 img:{
                     img1: QuarzoRosa1,
-                    img2: QuarzoRosa3,
-                    img3: QuarzoRosa2
+                    img2: QuarzoRosa2,
+                    img3: QuarzoRosa3
                 }
             },
 
             {   
                 id: 2,
+                name: 'Limpieza Facial',
+                subname: 'Cuarzo Amatista',
+                descp: "Experimenta el poder del cuarzo amatista para lograr una limpieza más profunda, extrayendo impurezas y regulando el flujo de oxígeno por tu piel.",
+                price: 150000,
+                img:{
+                    img1: Amatista1,
+                    img2: Amatista3,
+                    img3: Amatista2
+                }
+            },
+
+            {   
+                id: 3,
+                name: 'Limpieza Facial',
+                subname: 'Cuarzo Cristal',
+                descp: "Peeling ultrasónico, Nutrición facial, Masaje facial + vibración cuarzo cristal, mascarilla de alta hidratación.",
+                price: 130000,
+                img:{
+                    img1: Cristal1,
+                    img2: Cristal2,
+                    img3: Cristal3
+                }
+            },
+
+            {   
+                id: 4,
                 name: 'Hydrafacial Basic',
                 subname: 'Piel Mixta - Grasa',
                 descp: "Hydrafacial Es el más reciente avance en rejuvenecimiento de la piel. Es un procedimiento único que combina limpieza, exfoliación, extracción, hidratación y protección antioxidante.",
@@ -93,7 +122,7 @@ const ProductsContextProvider = ({children}) => {
             },
 
             {   
-                id: 3,
+                id: 5,
                 name: 'Hydrafacial Tonic',
                 descp: "Combina en una misma sesión limpieza, extracción de impurezas, peeling, e infiltración de sueros para obtener una piel de aspecto fresco, saludable, radiante y brillante.",
                 price: 180000,
@@ -105,7 +134,7 @@ const ProductsContextProvider = ({children}) => {
             },
 
             {   
-                id: 4,
+                id: 6,
                 name: 'Drenaje Linfático',
                 subname: 'Piernas cansadas',
                 descp: "Con este servicio, estarías combatiendo lesiones musculares, hinchazón, retención de líquido intersticial, y celulitis entre muchas otras cosas más. ",
@@ -118,7 +147,7 @@ const ProductsContextProvider = ({children}) => {
             },
 
             {   
-                id: 5,
+                id: 7,
                 name: 'Escleroterapia',
                 subname: 'Complemento',
                 descp: "Este servicio se ofrece por aparte, o en un paquete junto con nuestro Drenaje Linfático. ¡Mejora tu calidad de vida!",
@@ -127,32 +156,6 @@ const ProductsContextProvider = ({children}) => {
                     img1: Amatista1,
                     img2: Amatista2,
                     img3: Amatista3
-                }
-            },
-
-            {   
-                id: 6,
-                name: 'Limpieza Profunda',
-                subname: 'Cuarzo Amatista',
-                descp: "Experimenta el poder del cuarzo amatista para lograr una limpieza más profunda, extrayendo impurezas y regulando el flujo de oxígeno por tu piel.",
-                price: 150000,
-                img:{
-                    img1: Amatista1,
-                    img2: Amatista2,
-                    img3: Amatista3
-                }
-            },
-
-            {   
-                id: 7,
-                name: 'Limpieza Facial',
-                subname: 'Cuarzo Cristal',
-                descp: "Peeling ultrasónico, Nutrición facial, Masaje facial + vibración cuarzo cristal, mascarilla de alta hidratación.",
-                price: 130000,
-                img:{
-                    img1: Cristal3,
-                    img2: Cristal2,
-                    img3: Cristal1
                 }
             },
 
