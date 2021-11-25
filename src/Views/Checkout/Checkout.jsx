6cc5a1f7860da33d1b62dc39a7a2444ab5774145
jorgeduje <jorgeduje4@gmail.com>
2021-11-24 21:41:55 -0300

    paypal

--- src/Views/Checkout/Checkout.jsx
+++ src/Views/Checkout/Checkout.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-
+import React, {useContext} from "react";
+import ReactDOM from "react-dom";
 import "./Checkout.styles.css"
+import ProductContext from "../../Context/ProductsContext"
 
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 const Checkout = () => {
-    return (
-        <div className="contenedor-checkout">
-            <h2>Welcome to Checkout</h2>
-        </div>
-    )
-}
 
-export default Checkout
+    const {state} = useContext(ProductContext)
+
+  const createOrder = (data, actions) => {
+
+    return actions.order.create({
+
+        
+      purchase_units: [
+        {
+          amount: {
+            value: state.priceTotal,
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture();
+  };
+
+  const handlerPrice = () => {
+    
+  }
+
+  return (
+    <div className="container-paypal">
+      <PayPalButton
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+      />
+        <h3 className="text-center bg-black text-white py-2">{`Total: ${state.priceTotal}`}</h3>
+    </div>
+  );
+};
+
+export default Checkout;
